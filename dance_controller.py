7bfb7b2466466c66695775c7770ec0fd4cb3b267
moosanidaniyal <moosanidaniyal@gmail.com>
2026-06-19 17:21:49 +0100

    dance_controller movement
    moving into different poses

--- dance_controller.py
+++ dance_controller.py
@@ -3,8 +3,48 @@ from controller import Robot
 robot = Robot()
 timestep = int(robot.getBasicTimeStep())
 
-motor = robot.getDevice('LShoulderPitch')
-motor.setPosition(0.5)
+LShoulderPitch = robot.getDevice('LShoulderPitch')
+RShoulderPitch = robot.getDevice('RShoulderPitch')
+LShoulderRoll = robot.getDevice('LShoulderRoll')
+RShoulderRoll = robot.getDevice('RShoulderRoll')
+HeadYaw = robot.getDevice('HeadYaw')
+
+for i in range(100):
+    robot.step(timestep)
+
+print("MOVEMENT 1")
+LShoulderPitch.setPosition(-1.2)
+RShoulderPitch.setPosition(-1.2)
+for i in range(100):
+    robot.step(timestep)
+
+LShoulderPitch.setPosition(0.0)
+RShoulderPitch.setPosition(0.0)
+for i in range(100):
+    robot.step(timestep)
+
+print("MOVEMENT 2")
+LShoulderRoll.setPosition(1.0)
+RShoulderRoll.setPosition(-1.0)
+HeadYaw.setPosition(0.8)
+for i in range(100):
+    robot.step(timestep)
+
+LShoulderRoll.setPosition(0.0)
+RShoulderRoll.setPosition(0.0)
+HeadYaw.setPosition(0.0)
+for i in range(100):
+    robot.step(timestep)
+
+print("MOVEMENT 3")
+LShoulderPitch.setPosition(-0.8)
+RShoulderPitch.setPosition(-0.8)
+LShoulderRoll.setPosition(0.5)
+RShoulderRoll.setPosition(-0.5)
+for i in range(100):
+    robot.step(timestep)
+
+print("DONE")
 
 while robot.step(timestep) != -1:
     pass
